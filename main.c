981f2d2342cbf34492ea97f24d4f63fe3010bd50
phoebe-edward <phoebe.edward.kirelloss@gmail.com>
2023-09-24 16:18:36 -0700

    edited main

--- main.c
+++ main.c
@@ -42,7 +42,7 @@ int main(int argc, char **argv)
 		if (args != NULL)
 			free(args);
 		free_all(1);
-		fprintf(stderr, "Error: Can't open file %s\n", args->argv);
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
 	helping(args);
